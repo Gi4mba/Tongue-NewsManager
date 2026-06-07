9a28f95ab5bc964203fce3ae90da37ddd4e18922
Gi4mba <giamba.avella@gmail.com>
2026-06-07 19:48:52 +0200

    feat: implement constructor, modifier and addValidator function, style improvement

--- NewsManager.sol
+++ NewsManager.sol
@@ -7,40 +7,35 @@ import "./NewsValidationLib.sol";
 contract NewsManager {
 
     // ===== CUSTOM ERRORS =====
-
-    // Admin
     error OnlyOwner();
-    // Validators
-    error ValidatorNotFound();
-    error ValidatorAlreadyExists();
-    error ValidatorNotActive();
-    // News
-    error NewsNotFound();
-    error NewsExpired();
-    error NewsNotConfirmed();
-    error AlreadyValidated();
-    // Rating
+    
+    error ValidatorNotFound(address validator);
+    error ValidatorAlreadyExists(address validator);
+    error ValidatorNotActive(address validator);
+    
+    error NewsNotFound(address newsId);
+    error NewsExpired(address newsId);
+    error NewsNotConfirmed(address newsId);
+    error AlreadyValidated(address validator, address newsId);
+    
     error InvalidRating(uint8 vote);
-    // Reward Pool
+    
     error InsufficientBalance(uint256 required);
     
 
     // ===== EVENTS =====
-
-    // Admin Actions
-    event ValidatorAdded(address indexed _validator);
-    event ValidatorRemoved(address indexed _validator);
-    event RewardPoolToppedUp(address indexed _topper, uint256 _amount);
-    // News Actions
-    event NewsAdded(address indexed _newsId, string _newsName, uint256 _deadline);
-    event NewsValidated(address indexed _newsId, address indexed _validator, uint8 _rating);
-    event NewsConfirmed(address indexed _newsId);
-    // Reward Actions
-    event RewardDistributed(address indexed _validator, uint256 _amount);
+    event ValidatorAdded(address indexed validator);
+    event ValidatorRemoved(address indexed validator);
+    event RewardPoolToppedUp(address indexed topper, uint256 amount);
+    
+    event NewsAdded(address indexed newsId, string newsName, uint256 deadline);
+    event NewsValidated(address indexed newsId, address indexed validator, uint8 rating);
+    event NewsConfirmed(address indexed newsId);
+    
+    event RewardDistributed(address indexed validator, uint256 amount);
 
 
     // ===== STRUCTS =====
-
     struct Validator {
         uint256 accumulatedRewards;
         uint256 totalVotes;
@@ -56,23 +51,49 @@ contract NewsManager {
         uint256 validationCount;
         address[] validators;
     }
+
     
-
-    // ===== STATE VARIABLES =====
-
-    address public owner;
-    uint256 public totalDistributed;
-    uint256 public rewardAmount;
-
-
     // ===== MAPPINGS =====
-
     mapping(address => Validator) public validators;
     mapping(address => News) public newsList;
     
     
     // ===== ARRAYS =====
+    address[] public validatorsList;
 
-    address[] validatorsList;
+
+    // ===== STATE VARIABLES =====
+    address public owner;
+    uint256 public totalDistributed;
+    uint256 public rewardAmount;
+
+
+    // ===== CONSTRUCTOR =====
+    constructor(uint256 _rewardAmount) {
+        owner = msg.sender;
+        rewardAmount = _rewardAmount;
+        totalDistributed = 0;
+    }
+
+
+    // ===== MODIFIERS =====
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert OnlyOwner();
+        _;
+    }
+
+
+    // ===== CORE LOGIC =====
+
+    // AddValidator
+    function addValidator(address _validator) external onlyOwner {
+        if (NewsValidationLib.isValidator(validatorsList, _validator)) {
+            revert ValidatorAlreadyExists(_validator);
+        }
+
+        validators[_validator] = Validator(0, 0, 0, true);
+        validatorsList.push(_validator);
+        emit ValidatorAdded(_validator);
+    }
 
 }
--- NewsValidationLib.sol
+++ NewsValidationLib.sol
@@ -3,15 +3,15 @@
 pragma solidity >=0.8.3;
 
 library NewsValidationLib {
-    function getValidatorsCount(address[] storage _validators) internal view returns (uint256) {
-        return _validators.length;
+    function getValidatorsCount(address[] storage _validatorsList) internal view returns (uint256) {
+        return _validatorsList.length;
     }
 
-    function isValidator(address[] storage _validators, address _target) internal view returns (bool) {
+    function isValidator(address[] storage _validatorsList, address _target) internal view returns (bool) {
         for (
-            uint256 i = 0; i < _validators.length; i++
+            uint256 i = 0; i < _validatorsList.length; i++
         ) {
-            if (_validators[i] == _target) return true;
+            if (_validatorsList[i] == _target) return true;
         }
         return false;
     }
